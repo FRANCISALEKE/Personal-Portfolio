44aea57fdce6f9209749a2aa53ac998d331ee048
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2020-05-18 23:03:39 -0500

    portfolio done

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+    AOS.init({
+        duration: 1200,
+        once: true,
+      })
+
 });
